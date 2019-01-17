f32d3c0616314a8f7e83f6af92f24779fe6f6811
jdedwards3 <james.d.edwards.3@gmail.com>
2019-01-16 21:55:38 -0500

    removing unnecessary observables and public keyword

--- built/tictac.js
+++ built/tictac.js
@@ -10,28 +10,28 @@ class GridSquare {
 }
 class GameGrid {
     constructor() {
-        this.gridSquares = ko.observableArray(Array.from(Array(9), () => new GridSquare(ko.observable(new Mark(ko.observable())))));
-        this.columns = ["left", "mid", "right"].reduce((column, area, areaIndex) => (Object.assign({}, column, { [area]: this.gridSquares().filter((_, gridIndex) => gridIndex % 3 === areaIndex) })), {});
+        this.gridSquares = Array.from(Array(9), () => new GridSquare(new Mark(ko.observable())));
+        this.columns = ["left", "mid", "right"].reduce((column, area, areaIndex) => (Object.assign({}, column, { [area]: this.gridSquares.filter((_, gridIndex) => gridIndex % 3 === areaIndex) })), {});
         this.rows = ["top", "mid", "bottom"].reduce((row, area, areaIndex) => (Object.assign({}, row, { [area]: this.gridSquares.slice(areaIndex * 3, 3 + areaIndex * 3) })), {});
-        this.diagonals = ["left", "right"].reduce((diagonal, area, areaIndex) => (Object.assign({}, diagonal, { [area]: this.gridSquares().filter((_, gridIndex) => [[0, 4, 8], [2, 4, 6]][areaIndex].indexOf(gridIndex) > -1) })), {});
-        this.isWinner = ko.computed(() => [this.rows, this.columns, this.diagonals].some(component => Object.keys(component).some(area => component[area].every(gridSquare => gridSquare.mark().shape() &&
+        this.diagonals = ["left", "right"].reduce((diagonal, area, areaIndex) => (Object.assign({}, diagonal, { [area]: this.gridSquares.filter((_, gridIndex) => [[0, 4, 8], [2, 4, 6]][areaIndex].indexOf(gridIndex) > -1) })), {});
+        this.isWinner = ko.computed(() => [this.rows, this.columns, this.diagonals].some(component => Object.keys(component).some(area => component[area].every(gridSquare => gridSquare.mark.shape() &&
             ko.toJSON(gridSquare) === ko.toJSON(component[area][0])))));
     }
 }
 class TicTacViewModel {
     constructor() {
-        this.flipTurn = (shape) => ({ x: "o", o: "x", undefined: "x" }[shape]);
         this.updateTurn = (selectedGridSquare) => {
-            [!selectedGridSquare.mark().shape()]
+            [!selectedGridSquare.mark.shape()]
                 .filter(item => item && !this.gameGrid().isWinner())
                 .map(() => {
                 this.turn.shape(this.flipTurn(this.turn.shape()));
-                selectedGridSquare.mark().shape(this.turn.shape());
+                selectedGridSquare.mark.shape(this.turn.shape());
             });
         };
         this.resetGameGrid = () => {
             this.gameGrid(new GameGrid());
         };
+        this.flipTurn = (shape) => ({ x: "o", o: "x", undefined: "x" }[shape]);
         this.gameGrid = ko.observable(new GameGrid());
         this.turn = new Mark(ko.observable());
         this.getTurn = ko.computed(() => this.flipTurn(this.turn.shape()));
--- src/tictac.ts
+++ src/tictac.ts
@@ -9,28 +9,26 @@ class Mark {
 }
 
 class GridSquare {
-  constructor(public mark: KnockoutObservable<Mark>) {}
+  constructor(public mark: Mark) {}
 }
 
 class GameGrid {
-  public gridSquares: KnockoutObservableArray<GridSquare>;
-  public columns: IGridComponent;
-  public rows: IGridComponent;
-  public diagonals: IGridComponent;
-  public isWinner: KnockoutComputed<boolean>;
+  gridSquares: GridSquare[];
+  columns: IGridComponent;
+  rows: IGridComponent;
+  diagonals: IGridComponent;
+  isWinner: KnockoutComputed<boolean>;
 
   constructor() {
-    this.gridSquares = ko.observableArray(
-      Array.from(
-        Array(9),
-        () => new GridSquare(ko.observable(new Mark(ko.observable())))
-      )
+    this.gridSquares = Array.from(
+      Array(9),
+      () => new GridSquare(new Mark(ko.observable()))
     );
 
     this.columns = ["left", "mid", "right"].reduce(
       (column, area, areaIndex) => ({
         ...column,
-        [area]: this.gridSquares().filter(
+        [area]: this.gridSquares.filter(
           (_, gridIndex) => gridIndex % 3 === areaIndex
         )
       }),
@@ -48,7 +46,7 @@ class GameGrid {
     this.diagonals = ["left", "right"].reduce(
       (diagonal, area, areaIndex) => ({
         ...diagonal,
-        [area]: this.gridSquares().filter(
+        [area]: this.gridSquares.filter(
           (_, gridIndex) =>
             [[0, 4, 8], [2, 4, 6]][areaIndex].indexOf(gridIndex) > -1
         )
@@ -61,7 +59,7 @@ class GameGrid {
         Object.keys(component).some(area =>
           component[area].every(
             gridSquare =>
-              gridSquare.mark().shape() &&
+              gridSquare.mark.shape() &&
               ko.toJSON(gridSquare) === ko.toJSON(component[area][0])
           )
         )
@@ -71,9 +69,9 @@ class GameGrid {
 }
 
 class TicTacViewModel {
-  public gameGrid: KnockoutObservable<GameGrid>;
+  gameGrid: KnockoutObservable<GameGrid>;
+  getTurn: KnockoutComputed<string>;
   private turn: Mark;
-  public getTurn: KnockoutComputed<string>;
 
   constructor() {
     this.gameGrid = ko.observable(new GameGrid());
@@ -81,21 +79,21 @@ class TicTacViewModel {
     this.getTurn = ko.computed(() => this.flipTurn(this.turn.shape()));
   }
 
-  private flipTurn = (shape: string): string =>
-    ({ x: "o", o: "x", undefined: "x" }[shape]);
-
   updateTurn = (selectedGridSquare: GridSquare): void => {
-    [!selectedGridSquare.mark().shape()]
+    [!selectedGridSquare.mark.shape()]
       .filter(item => item && !this.gameGrid().isWinner())
       .map(() => {
         this.turn.shape(this.flipTurn(this.turn.shape()));
-        selectedGridSquare.mark().shape(this.turn.shape());
+        selectedGridSquare.mark.shape(this.turn.shape());
       });
   };
 
   resetGameGrid = (): void => {
     this.gameGrid(new GameGrid());
   };
+
+  private flipTurn = (shape: string): string =>
+    ({ x: "o", o: "x", undefined: "x" }[shape]);
 }
 
 ko.applyBindings(new TicTacViewModel());
